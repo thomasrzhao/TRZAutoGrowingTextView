53e15e8c821fba12e7c8adb0e6a699ce9b1240d2
Thomas Zhao <trzhao@stanford.edu>
2014-12-28 16:05:25 -0600

    Initial Commit

---
+++ TZAutoGrowingTextView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TZAutoGrowingTextView
+//
+//  Created by Thomas Zhao on 12/28/14.
+//  Copyright (c) 2014 thomasrzhao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TZAutoGrowingTextViewTests/TZAutoGrowingTextViewTests.swift
@@ -0,0 +1,36 @@
+//
+//  TZAutoGrowingTextViewTests.swift
+//  TZAutoGrowingTextViewTests
+//
+//  Created by Thomas Zhao on 12/28/14.
+//  Copyright (c) 2014 thomasrzhao. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class TZAutoGrowingTextViewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
